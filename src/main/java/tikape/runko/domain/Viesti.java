92a9457fd40def221202eb4c688ad8855cd24a38
sapetik <samu.tikkanen32@gmail.com>
2017-03-04 15:13:32 +0200

    Create Viesti.java

---
+++ src/main/java/tikape/runko/domain/Viesti.java
@@ -0,0 +1,48 @@
+
+package tikape.runko.domain;
+
+
+import java.sql.Timestamp;
+
+public class Viesti {
+    private Integer id;
+    private String viesti;
+    private String kayttaja;
+    private Timestamp aika;
+    private Integer viestiketju_id;
+    
+    public Viesti(Integer id, String viesti, String kayttaja, Timestamp aika, Integer viestiketju_id) {
+        this.id = id;
+        this.viesti = viesti;
+        this.kayttaja = kayttaja;
+        this.aika = aika;
+        this.viestiketju_id = viestiketju_id;
+    }   
+
+    public Integer getId() {
+        return id;
+    }
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNimi() {
+        return viesti;
+    }
+
+    public String getViesti() {
+        return viesti;
+    }
+
+    public String getKayttaja() {
+        return kayttaja;
+    }
+
+    public Timestamp getAika() {
+        return aika;
+    }
+    
+    
+}
+    
